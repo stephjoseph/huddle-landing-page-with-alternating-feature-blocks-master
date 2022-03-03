f9112c37769a212c8541b2546cad3fb0e127c4d0
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-04 00:40:16 +0800

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import CTA from "./CTA";
 
 const Main = () => {
   return (
-    <main className="z-10 flex w-[20.938rem] flex-col items-center gap-[7.5rem] xl:w-[88.88vw] xl:max-w-[80rem]">
+    <main className="z-10 flex w-[20.938rem] flex-col items-center gap-[7.5rem] xl:w-[88.88vw] xl:max-w-[80rem] xl:gap-[10rem]">
       <div className="flex w-full flex-col items-center gap-10">
         <Grow />
         <Conversations />
