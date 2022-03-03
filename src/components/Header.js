07cb5f684e551b9437a85c2a5329e1a982ab3c53
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-04 00:19:59 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import bgHeroMobile from "../images/bg-hero-mobile.svg";
+import bgHeroDesktop from "../images/bg-hero-desktop.svg";
 
 const Header = () => {
   return (
-    <header className="relative mb-[3.688rem] flex h-[45.063rem] w-full max-w-[26.75rem] flex-col items-center bg-[#EDFBFF]">
+    <header className="relative mb-[3.688rem] flex h-[45.063rem] w-full max-w-[26.75rem] flex-col items-center bg-[#EDFBFF] xl:mb-[10rem] xl:h-[51.45vw] xl:max-h-[46.313rem] xl:max-w-[90rem]">
       <div className="absolute left-0 top-0">
-        <img className="w-full" src={bgHeroMobile} alt="" />
+        <img className="w-full xl:hidden" src={bgHeroMobile} alt="" />
+        <img className="hidden w-full xl:block" src={bgHeroDesktop} alt="" />
       </div>
       <Navbar />
       <Hero />
