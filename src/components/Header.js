0d8e74fc8da9221032bbb4934e28f86cae93370e
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-03 15:57:36 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import bgHeroMobile from "../images/bg-hero-mobile.svg";
+import illustrationMockups from "../images/illustration-mockups.svg";
 
 const Header = () => {
   return (
-    <header className="flex w-full max-w-[26.75rem] flex-col items-center">
-      Header
-      <div className="flex w-[21.438rem] items-center justify-between">di</div>
+    <header className="relative flex h-[45.063rem] w-full max-w-[26.75rem] flex-col items-center bg-[#EDFBFF]">
+      <div className="absolute left-0 top-0 w-full">
+        <img className="w-full" src={bgHeroMobile} alt="" />
+      </div>
+      <Navbar />
+      <Hero />
     </header>
   );
 };
