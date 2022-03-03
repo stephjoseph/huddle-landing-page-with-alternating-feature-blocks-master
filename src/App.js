8543ac949aec74ab175236256e2d203b2874ec0e
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-03 19:50:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ const App = () => {
       <Header />
       <Main />
       <Footer />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua +1-543-123-4567
-      example@huddle.com About Us What We Do FAQ Career Blog Contact Us &copy;
-      Copyright 2018 Huddle. All rights reserved.
     </div>
   );
 };
