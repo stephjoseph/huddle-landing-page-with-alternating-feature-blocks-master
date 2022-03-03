fd9d7f16c3826d64efd400c3281c42e83c0f6989
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-03 17:11:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,10 @@ const App = () => {
       <Header />
       <Main />
       <Footer />
-      Flowing Conversations You wouldn't paginate a conversation in real life,
-      so why do it online? Our threads have just-in-time loading for a more
-      natural flow. Your Users It takes no time at all to integrate Huddle with
-      your app's authentication solution. This means, once signed in to your
-      app, your users can start chatting immediately. Ready To Build Your
-      Community? Get Started For Free Lorem ipsum dolor sit amet, consectetur
-      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua +1-543-123-4567 example@huddle.com About Us What We Do FAQ
-      Career Blog Contact Us &copy; Copyright 2018 Huddle. All rights reserved.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua +1-543-123-4567
+      example@huddle.com About Us What We Do FAQ Career Blog Contact Us &copy;
+      Copyright 2018 Huddle. All rights reserved.
     </div>
   );
 };
