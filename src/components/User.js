b884fe149e05052010accf961a285d5ed7242fcd
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-03 19:50:56 +0800

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import illustrationUser from "../images/illustration-your-users.svg";
 
 const User = () => {
   return (
-    <section className="flex w-full flex-col items-center gap-6 rounded-[16px] px-10  pb-[2.063rem] pt-6 shadow-[0px_0px_14px_0px_rgba(0,0,0,0.07)]">
+    <section className="flex w-full flex-col items-center gap-6 rounded-[16px] bg-white px-10  pb-[2.063rem] pt-6 shadow-[0px_0px_14px_0px_rgba(0,0,0,0.07)]">
       <div className="h-[16rem] w-[16rem]">
         <img src={illustrationUser} alt="" />
       </div>
