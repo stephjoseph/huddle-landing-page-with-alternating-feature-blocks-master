07beed09498213f8f12f00df6c9e1f6b12a71d98
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-03 15:15:11 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid place-items-center font-['Open_Sans']">
+      <Header />
+      <Main />
+      <Footer />
+      Try It Free Build The Community Your Fans Will Love Huddle re-imagines the
+      way we build communities. You have a voice, but so does your audience.
+      Create connections with your users as you engage in genuine discussion.
+      Get Started For Free Grow Together Generate meaningful discussions with
+      your audience and build a strong, loyal community. Think of the insightful
+      conversations you miss out on with a feedback form. Flowing Conversations
+      You wouldn't paginate a conversation in real life, so why do it online?
+      Our threads have just-in-time loading for a more natural flow. Your Users
+      It takes no time at all to integrate Huddle with your app's authentication
+      solution. This means, once signed in to your app, your users can start
+      chatting immediately. Ready To Build Your Community? Get Started For Free
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua +1-543-123-4567
+      example@huddle.com About Us What We Do FAQ Career Blog Contact Us &copy;
+      Copyright 2018 Huddle. All rights reserved.
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex w-full max-w-[26.75rem] flex-col items-center">
+      Header
+      <div className="flex w-[21.438rem] items-center justify-between">di</div>
+    </header>
+  );
+};
+
+export default Header;
